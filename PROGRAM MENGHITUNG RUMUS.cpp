a6165922afa5260446a11312b5127a464482e0c4
cici cemong <45526554+cicicemong@users.noreply.github.com>
2019-01-13 12:26:26 +0700

    Add files via upload

---
+++ PROGRAM MENGHITUNG RUMUS.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int E,m,c;
+    cout<<"PROGRAM MENGHITUNG RUMUS"<<endl;
+    cout<<"========================"<<endl;
+    cout<<"Masukan Nilai Massa           =";
+    cin>>m;
+    cout<<"Masukan Nilai Kekuatan Cahaya =";
+    cin>>c;
+    E=m*c*c;
+    cout<<"Energinya adalah              ="<<E;
+    return 0;
+}
